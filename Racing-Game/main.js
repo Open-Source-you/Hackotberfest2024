5e2dfb4cb69ffceaf1255a6e0b9339f69b4c7cd1
NightHack36 <Sodharmendra74@gmail.com>
2022-10-03 14:30:46 +0530

    Add Javascript

--- Racing-Game/main.js
+++ Racing-Game/main.js
@@ -1,154 +1,101 @@
-const playerOne = document.querySelector('.player.one');
-const playerTwo = document.querySelector('.player.two');
-const diceOne = document.querySelector('.dice.one');
-const diceTwo = document.querySelector('.dice.two');
-const rollDiceOne = document.querySelector('.rollDiceOne');
-const rollDiceTwo = document.querySelector('.rollDiceTwo');
-const titleOne = document.querySelector('.title.one');
-const titleTwo = document.querySelector('.title.two');
-const playerWinner = document.querySelector('.playerWinner');
-const finishLine = document.querySelector('.finishLine');
-let activePlayer = playerOne;
-let marginLeftOne = 0;
-let marginLeftTwo = 0;
+var scores, roundScore, activePlayer, dice, gamePlaying, lastDice;
+init();
 
-diceOne.style.background = 'url("img/dice-3.png") no-repeat center center';
-diceOne.style.backgroundSize = 'cover';
-diceTwo.style.background = 'url("img/dice-3.png") no-repeat center center';
-diceTwo.style.backgroundSize = 'cover';
 
-rollDiceOne.addEventListener('click', () => {
-    if (activePlayer === playerOne) {
-        let randomDiceNumber = Math.floor(Math.random() * 3 + 1);
-        diceOne.style.background = `url('img/dice-${randomDiceNumber}.png') no-repeat center center`;
-        diceOne.style.backgroundSize = 'cover';
-        marginLeftOne += randomDiceNumber;
-        titleOne.innerHTML = `You rolled ${randomDiceNumber}`;
-        if (marginLeftOne >= 8) {
-            marginLeftOne = 8;
-            playerOne.style.marginLeft = `${marginLeftOne * 180}px`;
-            playerWinner.innerHTML = `<h2 class="playerWinner">Player One has won! Congrats!</h2><button class="playAgain">Play again</button>`;
-            finishLine.classList.remove('hidden');
-            activePlayer = false;
-            document.body.addEventListener('click', e => {
-                if (e.target.classList.contains('playAgain')) {
-                    diceOne.style.background = 'url("img/dice-3.png") no-repeat center center';
-                    diceOne.style.backgroundSize = 'cover';
-                    diceTwo.style.background = 'url("img/dice-3.png") no-repeat center center';
-                    diceTwo.style.backgroundSize = 'cover';
-                    titleOne.innerHTML = '';
-                    titleTwo.innerHTML = '';
-                    playerWinner.innerHTML = '';
-                    playerOne.style.marginLeft = `${marginLeftOne * 0}px`;
-                    playerTwo.style.marginLeft = `${marginLeftTwo * 0}px`;
-                    activePlayer = playerOne;
-                    rollDiceOne.classList.add("active");
-                    rollDiceTwo.classList.remove("active");
-                    marginLeftOne = 0;
-                    marginLeftTwo = 0;
-                    playerOne.style.marginLeft = `${marginLeftOne * 180}px`;
-                    playerTwo.style.marginLeft = `${marginLeftTwo * 180}px`;
-                    finishLine.classList.add('hidden');
-                }
-            });
-            return;
-        } else {
-            if (marginLeftOne === 3 || marginLeftOne === 6) {
-                if (marginLeftOne === 3) {
-                    marginLeftOne = 3;
-                    playerOne.style.marginLeft = `${marginLeftOne * 180}px`;
-                    activePlayer = playerTwo;
-                    rollDiceOne.classList.remove("active");
-                    rollDiceTwo.classList.add("active");
-                    setTimeout(() => {
-                        marginLeftOne -= 1;
-                        playerOne.style.marginLeft = `${marginLeftOne * 180}px`;
-                    }, 1000);
-                } else if (marginLeftOne === 6) {
-                    marginLeftOne = 6;
-                    playerOne.style.marginLeft = `${marginLeftOne * 180}px`;
-                    activePlayer = playerTwo;
-                    rollDiceOne.classList.remove("active");
-                    rollDiceTwo.classList.add("active");
-                    setTimeout(() => {
-                        marginLeftOne -= 1;
-                        playerOne.style.marginLeft = `${marginLeftOne * 180}px`;
-                    }, 1000);
-                }
-            } else {
-                playerOne.style.marginLeft = `${marginLeftOne * 180}px`;
-                activePlayer = playerTwo;
-                rollDiceOne.classList.remove("active");
-                rollDiceTwo.classList.add("active");
-            }
-        }
-    }
+document.querySelector('.btn-roll').addEventListener('click', function(){
+	if (gamePlaying){
+		let dice = Math.floor(Math.random() * 6) + 1;
+		let diceDOM = document.querySelector('.dice');
+
+		// Display result
+		diceDOM.style.display = 'block';
+		diceDOM.src = 'dice-' + dice + '.png';
+
+		// Update round score if rolled num !== 1
+		if (dice === 6 && lastDice === 6){
+			// player looses score
+			scores[activePlayer] = 0;
+			document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+			nextPlayer();
+		} else if(dice !== 1){
+			// Add score
+			roundScore += dice;
+			document.querySelector('#current-' + activePlayer).textContent = roundScore;
+		} else {
+			// Next player
+			nextPlayer();
+		}
+
+	  lastDice = dice;
+	}
 });
 
-rollDiceTwo.addEventListener('click', () => {
-    if (activePlayer === playerTwo) {
-        let randomDiceNumber = Math.floor(Math.random() * 3 + 1);
-        diceTwo.style.background = `url('img/dice-${randomDiceNumber}.png') no-repeat center center`;
-        diceTwo.style.backgroundSize = 'cover';
-        marginLeftTwo += randomDiceNumber;
-        titleTwo.innerHTML = `You rolled ${randomDiceNumber}`;
-        if (marginLeftTwo >= 8) {
-            marginLeftTwo = 8;
-            playerTwo.style.marginLeft = `${marginLeftTwo * 180}px`;
-            playerWinner.innerHTML = `<h2 class="playerWinner">Player Two has won! Congrats!</h2><button class="playAgain">Play again</button>`;
-            finishLine.classList.remove('hidden');
-            activePlayer = false;
-            document.body.addEventListener('click', e => {
-                if (e.target.classList.contains('playAgain')) {
-                    diceOne.style.background = 'url("img/dice-3.png") no-repeat center center';
-                    diceOne.style.backgroundSize = 'cover';
-                    diceTwo.style.background = 'url("img/dice-3.png") no-repeat center center';
-                    diceTwo.style.backgroundSize = 'cover';
-                    titleOne.innerHTML = '';
-                    titleTwo.innerHTML = '';
-                    playerWinner.innerHTML = '';
-                    playerOne.style.marginLeft = `${marginLeftOne * 0}px`;
-                    playerTwo.style.marginLeft = `${marginLeftTwo * 0}px`;
-                    activePlayer = playerOne;
-                    rollDiceOne.classList.add("active");
-                    rollDiceTwo.classList.remove("active");
-                    marginLeftOne = 0;
-                    marginLeftTwo = 0;
-                    playerOne.style.marginLeft = `${marginLeftOne * 180}px`;
-                    playerTwo.style.marginLeft = `${marginLeftTwo * 180}px`;
-                    finishLine.classList.add('hidden');
-                }
-            });
-            return;
-        } else {
-            if (marginLeftTwo === 2 || marginLeftTwo === 5) {
-                if (marginLeftTwo === 2) {
-                    marginLeftTwo = 2;
-                    playerTwo.style.marginLeft = `${marginLeftTwo * 180}px`;
-                    activePlayer = playerOne;
-                    rollDiceOne.classList.add("active");
-                    rollDiceTwo.classList.remove("active");
-                    setTimeout(() => {
-                        marginLeftTwo -= 1;
-                        playerTwo.style.marginLeft = `${marginLeftTwo * 180}px`;
-                    }, 1000);
-                } else if (marginLeftTwo === 5) {
-                    marginLeftTwo = 5;
-                    playerTwo.style.marginLeft = `${marginLeftTwo * 180}px`;
-                    activePlayer = playerOne;
-                    rollDiceOne.classList.add("active");
-                    rollDiceTwo.classList.remove("active");
-                    setTimeout(() => {
-                        marginLeftTwo -= 1;
-                        playerTwo.style.marginLeft = `${marginLeftTwo * 180}px`;
-                    }, 1000);
-                }
-            } else {
-                playerTwo.style.marginLeft = `${marginLeftTwo * 180}px`;
-                activePlayer = playerOne;
-                rollDiceOne.classList.add("active");
-                rollDiceTwo.classList.remove("active");
-            }
-        }
-    }
+document.querySelector('.btn-hold').addEventListener('click', function(){
+	if (gamePlaying){
+		// Add current score to global score
+		scores[activePlayer] += roundScore;
+		// Update UI
+		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+		
+		let input = document.querySelector('.final-score').value;
+		let winningScore;
+		
+		if(input){
+			winningScore = input;
+		} else {
+			winningScore = 100;
+		}
+
+		// Check if player won the game
+		if (scores[activePlayer] >= input){
+			document.querySelector('#name-' + activePlayer).textContent = 'WINNER!...';
+			document.querySelector('.dice').style.display = 'none';
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.	querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+			gamePlaying = false;
+		} else {
+			nextPlayer();
+		}	
+	}
+	
 });
+
+function nextPlayer(){
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+	
+	document.querySelector('.dice').style.display = 'none';
+};
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+	scores = [0, 0];
+	activePlayer = 0;
+	roundScore = 0;
+	gamePlaying = true;
+
+	document.querySelector('.dice').style.display = 'none';
+
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+
+	document.querySelector('.player-0-panel').classList.add('active');
+}
