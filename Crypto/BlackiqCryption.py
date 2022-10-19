d58f2d64ec319fc03773975ad42bdb9344206b22
srinidh-007 <srinidhreddyyeldhandi123@gmail.com>
2022-10-19 17:14:56 +0530

    Crypto

---
+++ Crypto/BlackiqCryption.py
@@ -0,0 +1,493 @@
+print("Welcome to BlackIQCryption")
+
+print("Cryption - Encroption")
+ask = str(input("What do you want ? C or E : "))
+
+# Cryption
+if ask == "C":
+    # data
+    A = a = "!"
+    B = b = "@"
+    C = c = "#"
+    D = d = "$"
+    E = e = "%"
+    F = f = "^"
+    G = g = "&"
+    H = h = "*"
+    I = i = "("
+    J = j = "-"
+    K = k = "+"
+    L = l = "="
+    M = m = "_"
+    N = n = "/"
+    O = o = "|"
+    P = p = "}"
+    Q = q = "{"
+    R = r = "]"
+    S = s = "["
+    T = t = ")"
+    U = u = ":"
+    V = v = ";"
+    W = w = ","
+    X = x = ">"
+    Y = y = "<"
+    Z = z = "'"
+
+    # input
+    text = str(input("Give me : "))
+
+    # coding
+    for chap in text:
+        # Smalls
+        if chap == "a":
+            print(a)
+        if chap == "b":
+            print(b)
+        if chap == "c":
+            print(c)
+        if chap == "d":
+            print(d)
+        if chap == "e":
+            print(e)
+        if chap == "f":
+            print(f)
+        if chap == "g":
+            print(g)
+        if chap == "h":
+            print(h)
+        if chap == "i":
+            print(i)
+        if chap == "j":
+            print(j)
+        if chap == "k":
+            print(k)
+        if chap == "l":
+            print(l)
+        if chap == "m":
+            print(m)
+        if chap == "n":
+            print(n)
+        if chap == "o":
+            print(o)
+        if chap == "p":
+            print(p)
+        if chap == "q":
+            print(q)
+        if chap == "r":
+            print(r)
+        if chap == "s":
+            print(s)
+        if chap == "t":
+            print(t)
+        if chap == "u":
+            print(u)
+        if chap == "v":
+            print(v)
+        if chap == "w":
+            print(w)
+        if chap == "x":
+            print(x)
+        if chap == "y":
+            print(y)
+        if chap == "z":
+            print(z)
+        # Capitals
+        if chap == "A":
+            print(a)
+        if chap == "B":
+            print(b)
+        if chap == "C":
+            print(c)
+        if chap == "D":
+            print(d)
+        if chap == "E":
+            print(e)
+        if chap == "F":
+            print(f)
+        if chap == "G":
+            print(g)
+        if chap == "H":
+            print(h)
+        if chap == "I":
+            print(i)
+        if chap == "J":
+            print(j)
+        if chap == "K":
+            print(k)
+        if chap == "L":
+            print(l)
+        if chap == "M":
+            print(m)
+        if chap == "N":
+            print(n)
+        if chap == "O":
+            print(o)
+        if chap == "P":
+            print(p)
+        if chap == "Q":
+            print(q)
+        if chap == "R":
+            print(r)
+        if chap == "S":
+            print(s)
+        if chap == "T":
+            print(t)
+        if chap == "U":
+            print(u)
+        if chap == "V":
+            print(v)
+        if chap == "W":
+            print(w)
+        if chap == "X":
+            print(x)
+        if chap == "Y":
+            print(y)
+        if chap == "Z":
+            print(z)
+        # Others
+        if chap == " ":
+            print(" ")
+        if chap == ".":
+            print(".")
+
+    # end
+    exiting = input()
+# cryption
+if ask == "c":
+    # data
+    A = a = "!"
+    B = b = "@"
+    C = c = "#"
+    D = d = "$"
+    E = e = "%"
+    F = f = "^"
+    G = g = "&"
+    H = h = "*"
+    I = i = "("
+    J = j = "-"
+    K = k = "+"
+    L = l = "="
+    M = m = "_"
+    N = n = "/"
+    O = o = "|"
+    P = p = "}"
+    Q = q = "{"
+    R = r = "]"
+    S = s = "["
+    T = t = ")"
+    U = u = ":"
+    V = v = ";"
+    W = w = ","
+    X = x = ">"
+    Y = y = "<"
+    Z = z = "'"
+
+    # input
+    text = str(input("Give me : "))
+
+    # coding
+    for chap in text:
+        # Smalls
+        if chap == "a":
+            print(a)
+        if chap == "b":
+            print(b)
+        if chap == "c":
+            print(c)
+        if chap == "d":
+            print(d)
+        if chap == "e":
+            print(e)
+        if chap == "f":
+            print(f)
+        if chap == "g":
+            print(g)
+        if chap == "h":
+            print(h)
+        if chap == "i":
+            print(i)
+        if chap == "j":
+            print(j)
+        if chap == "k":
+            print(k)
+        if chap == "l":
+            print(l)
+        if chap == "m":
+            print(m)
+        if chap == "n":
+            print(n)
+        if chap == "o":
+            print(o)
+        if chap == "p":
+            print(p)
+        if chap == "q":
+            print(q)
+        if chap == "r":
+            print(r)
+        if chap == "s":
+            print(s)
+        if chap == "t":
+            print(t)
+        if chap == "u":
+            print(u)
+        if chap == "v":
+            print(v)
+        if chap == "w":
+            print(w)
+        if chap == "x":
+            print(x)
+        if chap == "y":
+            print(y)
+        if chap == "z":
+            print(z)
+        # Capitals
+        if chap == "A":
+            print(a)
+        if chap == "B":
+            print(b)
+        if chap == "C":
+            print(c)
+        if chap == "D":
+            print(d)
+        if chap == "E":
+            print(e)
+        if chap == "F":
+            print(f)
+        if chap == "G":
+            print(g)
+        if chap == "H":
+            print(h)
+        if chap == "I":
+            print(i)
+        if chap == "J":
+            print(j)
+        if chap == "K":
+            print(k)
+        if chap == "L":
+            print(l)
+        if chap == "M":
+            print(m)
+        if chap == "N":
+            print(n)
+        if chap == "O":
+            print(o)
+        if chap == "P":
+            print(p)
+        if chap == "Q":
+            print(q)
+        if chap == "R":
+            print(r)
+        if chap == "S":
+            print(s)
+        if chap == "T":
+            print(t)
+        if chap == "U":
+            print(u)
+        if chap == "V":
+            print(v)
+        if chap == "W":
+            print(w)
+        if chap == "X":
+            print(x)
+        if chap == "Y":
+            print(y)
+        if chap == "Z":
+            print(z)
+        # Others
+        if chap == " ":
+            print(" ")
+        if chap == ".":
+            print(".")
+
+    # end
+    exiting = input()
+# Encryotion
+if ask == "E":
+    # data
+    etajabo = "a"
+    ead = "b"
+    esharp = "c"
+    edolar = "d"
+    edarsad = "e"
+    etavan = "f"
+    eand = "g"
+    esetare = "h"
+    eparbaz = "i"
+    emidline = "j"
+    eplus = "k"
+    eeq = "l"
+    eunderline = "m"
+    eeslash = "n"
+    eras = "o"
+    eocbas = "p"
+    eocbaz = "q"
+    ecrobas = "r"
+    ecrobaz = "s"
+    eocbaz = "t"
+    edonogh = "u"
+    esemi = "v"
+    ecama = "w"
+    ehashbas = "x"
+    ehashbaz = "y"
+    eup = "z"
+
+    # input
+    text = str(input("Give me : "))
+
+    # coding
+    for chap in text:
+        if chap == "!":
+            print(etajabo)
+        if chap == "@":
+            print(ead)
+        if chap == "#":
+            print(esharp)
+        if chap == "$":
+            print(edolar)
+        if chap == "%":
+            print(edarsad)
+        if chap == "^":
+            print(etavan)
+        if chap == "&":
+            print(eand)
+        if chap == "*":
+            print(esetare)
+        if chap == "(":
+            print(eparbaz)
+        if chap == "-":
+            print(emidline)
+        if chap == "+":
+            print(eplus)
+        if chap == "=":
+            print(eeq)
+        if chap == "_":
+            print(eunderline)
+        if chap == "/":
+            print(eeslash)
+        if chap == "|":
+            print(eras)
+        if chap == "}":
+            print(eocbas)
+        if chap == "{":
+            print(eocbaz)
+        if chap == "]":
+            print(ecrobas)
+        if chap == "[":
+            print(ecrobaz)
+        if chap == ")":
+            print(eocbas)
+        if chap == ":":
+            print(edonogh)
+        if chap == ";":
+            print(esemi)
+        if chap == ",":
+            print(ecama)
+        if chap == ">":
+            print(ehashbas)
+        if chap == "<":
+            print(ehashbaz)
+        if chap == "'":
+            print(eup)
+        if chap == " ":
+            print(" ")
+        if chap == ".":
+            print(".")
+
+    # end
+    exiting = input()
+# encryption
+if ask == "e":
+    # data
+    etajabo = "a"
+    ead = "b"
+    esharp = "c"
+    edolar = "d"
+    edarsad = "e"
+    etavan = "f"
+    eand = "g"
+    esetare = "h"
+    eparbaz = "i"
+    emidline = "j"
+    eplus = "k"
+    eeq = "l"
+    eunderline = "m"
+    eeslash = "n"
+    eras = "o"
+    eocbas = "p"
+    eocbaz = "q"
+    ecrobas = "r"
+    ecrobaz = "s"
+    eocbaz = "t"
+    edonogh = "u"
+    esemi = "v"
+    ecama = "w"
+    ehashbas = "x"
+    ehashbaz = "y"
+    eup = "z"
+
+    # input
+    text = str(input("Give me : "))
+
+    # coding
+    for chap in text:
+        if chap == "!":
+            print(etajabo)
+        if chap == "@":
+            print(ead)
+        if chap == "#":
+            print(esharp)
+        if chap == "$":
+            print(edolar)
+        if chap == "%":
+            print(edarsad)
+        if chap == "^":
+            print(etavan)
+        if chap == "&":
+            print(eand)
+        if chap == "*":
+            print(esetare)
+        if chap == "(":
+            print(eparbaz)
+        if chap == "-":
+            print(emidline)
+        if chap == "+":
+            print(eplus)
+        if chap == "=":
+            print(eeq)
+        if chap == "_":
+            print(eunderline)
+        if chap == "/":
+            print(eeslash)
+        if chap == "|":
+            print(eras)
+        if chap == "}":
+            print(eocbas)
+        if chap == "{":
+            print(eocbaz)
+        if chap == "]":
+            print(ecrobas)
+        if chap == "[":
+            print(ecrobaz)
+        if chap == ")":
+            print(eocbas)
+        if chap == ":":
+            print(edonogh)
+        if chap == ";":
+            print(esemi)
+        if chap == ",":
+            print(ecama)
+        if chap == ">":
+            print(ehashbas)
+        if chap == "<":
+            print(ehashbaz)
+        if chap == "'":
+            print(eup)
+        if chap == " ":
+            print(" ")
+        if chap == ".":
+            print(".")
+
+    # end
+    exiting = input()
+
+print("Done !")
